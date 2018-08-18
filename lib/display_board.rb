301b186f245fcfdf5700234934ab60675d6da146
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-18 14:49:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  cell = "   "
+  horizontal_separator = "-----------"
+  row = "#{cell}|#{cell}|#{cell}"
+  board = "#{row}\n#{horizontal_separator}\n#{row}\n#{horizontal_separator}\n#{row}\n"
+  "   |   |   
+  -----------
+     |   |   
+     -----------
+        |   |   
+        "
+end
